5762da29c9a59c25de150cacf4f61acaddd454fb
NHAT100 <48667951+buitruongnhat100@users.noreply.github.com>
2020-04-18 14:35:09 +0700

    Update Program.cs
    First commit

--- NCopy/Program.cs
+++ NCopy/Program.cs
@@ -17,6 +17,7 @@ namespace NCopy
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // first commit
         }
     }
 }
