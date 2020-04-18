f790d64a866c8be74e50d0e126e4f0990a4501ea
NHAT100 <buitruongnhatlm@gmail.com>
2020-04-18 14:37:05 +0700

    Second commit

--- NCopy/Program.cs
+++ NCopy/Program.cs
@@ -17,7 +17,6 @@ namespace NCopy
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            // first commit
         }
     }
 }
